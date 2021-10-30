62b0c18e3ae158b41536f68305029b15f7e8b7ec
sawwere <sawer_13@mail.ru>
2021-10-30 14:17:07 +0300

    add LaterInYear function

--- main.cpp
+++ main.cpp
@@ -1,12 +1,36 @@
+#include <iostream>
+
+
 bool IsLeapYear(int year)
 {
 	return (year % 4 == 0) && !((year % 100 == 0) && (year % 400 != 0));
 }
 
-int SecondsInMinute()
+void LaterInYear(int m1, int d1, int m2, int d2)
 {
-	return 60;
+	if (m1 > m2)
+		std::cout << "first date";
+	else
+	{
+		if (m1 == m2)
+		{
+			if (d1 > d2)
+			{
+				std::cout << "first date";
+			}
+			else
+			{
+				if (d1 == d2)
+					std::cout << "equally";
+				else
+					std::cout << "second date";
+			}
+		}
+		else
+			std::cout << "second date";
+	}
 }
+
 
 int main()
 {
